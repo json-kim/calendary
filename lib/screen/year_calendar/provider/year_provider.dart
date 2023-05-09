485f77be7541c219fb300fb95d936e59b3c0ad3a
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-09 20:51:27 +0900

    Update YearProvider 수정 DateTime -> int

--- lib/screen/year_calendar/provider/year_provider.dart
+++ lib/screen/year_calendar/provider/year_provider.dart
@@ -1,36 +1,35 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:dart_date/dart_date.dart';
 import 'package:flutter_todaily/util/const_value.dart';
 
 /// 메인 캘린더 화면 (연도 선택)
 /// 선택된 연도 상태 관리
-class YearNotifier extends Notifier<DateTime> {
+class YearNotifier extends Notifier<int> {
   @override
-  DateTime build() {
-    return DateTime.now();
+  int build() {
+    return DateTime.now().year;
   }
 
   /// 이전 연도 선택
   void beforeYear() {
-    if (state.year < 2022) {
+    if (state < 2022) {
       return;
     }
 
-    state = state.subYears(1);
+    state = state--;
   }
 
   /// 다음 연도 선택
   void nextYear() {
     final maxYear = DateTime.now().year + maxYearTerm;
 
-    if (state.year >= maxYear) {
+    if (state >= maxYear) {
       return;
     }
 
-    state = state.addYears(1);
+    state = state++;
   }
 }
 
-final yearProvider = NotifierProvider<YearNotifier, DateTime>(() {
+final yearProvider = NotifierProvider<YearNotifier, int>(() {
   return YearNotifier();
 });
