9241cfa8a7eb0ce4315d88b0fb4095bb61b48e79
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-14 22:23:42 +0900

    Add Calendars 테이블 DAO 클래스 추가

---
+++ lib/data/data_source/local/dao/calendars_dao.dart
@@ -0,0 +1,52 @@
+import 'package:drift/drift.dart';
+import 'package:flutter_todaily/data/data_source/local/database/local_database.dart';
+import 'package:flutter_todaily/data/data_source/local/table/calendar_table.dart';
+
+part 'calendars_dao.g.dart';
+
+@DriftAccessor(tables: [Calendars])
+class CalendarsDao extends DatabaseAccessor<LocalDatabase>
+    with _$CalendarsDaoMixin {
+  CalendarsDao(super.attachedDatabase);
+
+  /// 캘린더 삽입
+  Future<int> insertCalendar(CalendarsCompanion calendar) {
+    return into(calendars).insert(calendar, mode: InsertMode.insertOrReplace);
+  }
+
+  /// 캘린더 제거
+  Future<int> deleteCalendar(int id) async {
+    return (delete(calendars)..where((tbl) => tbl.id.equals(id))).go();
+  }
+
+  /// 캘린더 수정
+  Future<int> updateCalendar(CalendarsCompanion newCalendar) async {
+    return (update(calendars)
+          ..where((tbl) => tbl.id.equals(newCalendar.id.value)))
+        .write(newCalendar);
+  }
+
+  /// 캘린더 가져오기 with year
+  Future<List<Calendar>> selectCalendarListInYear(int year) {
+    return (select(calendars)..where((tbl) => tbl.date.year.equals(year)))
+        .get();
+  }
+
+  /// 캘린더 가져오기 with month
+  Future<List<Calendar>> selectCalendarListInMonth(int month) {
+    return (select(calendars)..where((tbl) => tbl.date.month.equals(month)))
+        .get();
+  }
+
+  /// 캘린더 가져오기 with date
+  Future<Calendar?> selectCalendarOfDate(DateTime otherDate) {
+    return (select(calendars)..where((tbl) => tbl.isSameDate(otherDate)))
+        .getSingleOrNull();
+  }
+
+  /// 캘린더 가져오기 with id
+  Future<Calendar?> selectCalendarWithId(int id) {
+    return (select(calendars)..where((tbl) => tbl.id.equals(id)))
+        .getSingleOrNull();
+  }
+}
