c98d0fad18b983c322d571a1658b1fd986022fc8
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-14 13:35:17 +0900

    Add 캘린더 테이블 생성

---
+++ lib/data/data_source/local/table/calendar_table.dart
@@ -0,0 +1,10 @@
+import 'package:drift/drift.dart';
+import 'package:flutter_todaily/model/enum/calendar_mood_enum.dart';
+
+class Calendars extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text().withLength(min: 2, max: 40)();
+  TextColumn get content => text().withLength(max: 1000).nullable()();
+  IntColumn get mood => intEnum<CalendarMood>()();
+  DateTimeColumn get date => dateTime()();
+}
