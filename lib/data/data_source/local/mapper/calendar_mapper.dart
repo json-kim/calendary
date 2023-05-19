5e6f3e21efa9a0d9627717dbe7074505775bcffb
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-19 20:12:31 +0900

    Add EditCalendarMapper 확장함수 추가

--- lib/data/data_source/local/mapper/calendar_mapper.dart
+++ lib/data/data_source/local/mapper/calendar_mapper.dart
@@ -1,9 +1,10 @@
 import 'package:drift/drift.dart';
 import 'package:flutter_todaily/data/data_source/local/database/local_database.dart';
 import 'package:flutter_todaily/model/calendar_model.dart';
+import 'package:flutter_todaily/model/edit_calendar_model.dart';
 
 extension CalendarModelMapper on CalendarModel {
-  /// 캘린더 모델을 DB에 삽입할 수 있는 모델로 변경하는 함수
+  /// 캘린더 모델을 DB에 삽입할 수 있는 객체로 변환하는 함수
   CalendarsCompanion toCompanion() {
     return CalendarsCompanion.insert(
         id: Value(id),
@@ -14,8 +15,16 @@ extension CalendarModelMapper on CalendarModel {
   }
 }
 
+extension EditCalendarModelMapper on EditCalendarModel {
+  /// 캘린더 생성 모델을 DB에 삽입할 수 있는 객체로 변환하는 함수
+  CalendarsCompanion toCompanion() {
+    return CalendarsCompanion.insert(
+        title: title, mood: mood, date: date, content: Value(content));
+  }
+}
+
 extension CalendarEntityMapper on Calendar {
-  /// 캘린더 엔티티(Dao 결과값)을 캘린더 모델로 변경하는 함수
+  /// 캘린더 엔티티(Dao 결과값)을 캘린더 모델로 변환하는 함수
   CalendarModel toModel() {
     return CalendarModel(
         id: id, title: title, content: content, date: date, mood: mood);
