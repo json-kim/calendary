120f5a7f46dbf01c8c69c5699621e7e2124e55a6
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-20 15:00:11 +0900

    Refactor 테스트를 위해 캘린더 Dao를 추상화한다

--- lib/data/data_source/local/dao/calendars_dao.dart
+++ lib/data/data_source/local/dao/calendars_dao.dart
@@ -4,22 +4,37 @@ import 'package:flutter_todaily/data/data_source/local/table/calendar_table.dart
 
 part 'calendars_dao.g.dart';
 
+abstract class CalendarsDaoType {
+  Future<int> insertCalendar(CalendarsCompanion calendar);
+  Future<int> deleteCalendar(int id);
+  Future<int> updateCalendar(CalendarsCompanion newCalendar);
+  Future<List<Calendar>> selectCalendarListInYear(int year);
+  Future<List<Calendar>> selectCalendarListInMonth(int month);
+  Future<Calendar?> selectCalendarOfDate(DateTime otherDate);
+  Future<Calendar?> selectCalendarWithId(int id);
+  Future<int> deleteCalendarAll();
+}
+
 @DriftAccessor(tables: [Calendars])
 class CalendarsDao extends DatabaseAccessor<LocalDatabase>
-    with _$CalendarsDaoMixin {
+    with _$CalendarsDaoMixin
+    implements CalendarsDaoType {
   CalendarsDao(super.attachedDatabase);
 
   /// 캘린더 삽입
+  @override
   Future<int> insertCalendar(CalendarsCompanion calendar) {
     return into(calendars).insert(calendar, mode: InsertMode.insertOrReplace);
   }
 
   /// 캘린더 제거
+  @override
   Future<int> deleteCalendar(int id) async {
     return (delete(calendars)..where((tbl) => tbl.id.equals(id))).go();
   }
 
   /// 캘린더 수정
+  @override
   Future<int> updateCalendar(CalendarsCompanion newCalendar) async {
     return (update(calendars)
           ..where((tbl) => tbl.id.equals(newCalendar.id.value)))
@@ -27,6 +42,7 @@ class CalendarsDao extends DatabaseAccessor<LocalDatabase>
   }
 
   /// 캘린더 가져오기 with year
+  @override
   Future<List<Calendar>> selectCalendarListInYear(int year) {
     return (select(calendars)
           ..where((tbl) => tbl.date
@@ -37,6 +53,7 @@ class CalendarsDao extends DatabaseAccessor<LocalDatabase>
   }
 
   /// 캘린더 가져오기 with month
+  @override
   Future<List<Calendar>> selectCalendarListInMonth(int month) {
     return (select(calendars)
           ..where((tbl) => tbl.date
@@ -47,18 +64,21 @@ class CalendarsDao extends DatabaseAccessor<LocalDatabase>
   }
 
   /// 캘린더 가져오기 with date
+  @override
   Future<Calendar?> selectCalendarOfDate(DateTime otherDate) {
     return (select(calendars)..where((tbl) => tbl.isSameDate(otherDate)))
         .getSingleOrNull();
   }
 
   /// 캘린더 가져오기 with id
+  @override
   Future<Calendar?> selectCalendarWithId(int id) {
     return (select(calendars)..where((tbl) => tbl.id.equals(id)))
         .getSingleOrNull();
   }
 
   /// 캘린더 전부 제거하기
+  @override
   Future<int> deleteCalendarAll() {
     return delete(calendars).go();
   }
