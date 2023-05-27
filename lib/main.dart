29b3ed4968b93e280495fe3579d4da34e4d9e6b8
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-27 23:20:50 +0900

    Add 머티리얼 앱에 로컬라이징 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todaily/provider/util/provider_logger.dart';
+import 'package:flutter_todaily/screen/edit_calendar/edit_calendar_screen.dart';
 import 'package:flutter_todaily/screen/year_calendar/main_calendar_screen.dart';
 
 void main() {
   runApp(ProviderScope(observers: [ProviderLogger()], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
       title: 'todaily',
-      home: MainCalendarScreen(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko'), // Koread
+      ],
+      theme: ThemeData(useMaterial3: true),
+      home: EditCalendarScreen(),
     );
   }
 }
