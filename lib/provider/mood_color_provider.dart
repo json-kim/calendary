ba91211dd20cff406d5baf99faa59ac844960036
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-27 20:28:32 +0900

    Update 무드 컬러 테마 프로바이더 네임 추가

--- lib/provider/mood_color_provider.dart
+++ lib/provider/mood_color_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todaily/util/mood_color_theme.dart';
 
-final moodColorThemeProvider =
-    Provider<MoodColorTheme>((ref) => defaultColorTheme);
+final moodColorThemeProvider = Provider<MoodColorTheme>(
+    (ref) => defaultColorTheme,
+    name: 'MoodColorThemeProvider');
