2a4aac14bb003884b7f46da2434b1edc33c89863
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-28 13:43:36 +0900

    Add 메인 캘린더 화면 pull to refresh 추가

--- lib/screen/year_calendar/widget/year_calendar.dart
+++ lib/screen/year_calendar/widget/year_calendar.dart
@@ -1,19 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_todaily/provider/calendar_list_provider.dart';
 import 'package:flutter_todaily/screen/year_calendar/widget/dates_grid.dart';
 
-class YearCalendar extends StatelessWidget {
+class YearCalendar extends ConsumerWidget {
   const YearCalendar({super.key});
 
+  Future<void> refresh(WidgetRef ref) async {
+    ref.refresh(calendarListProvider);
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            /// 날짜 그리드
-            DatesGrid(),
-          ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RefreshIndicator(
+        onRefresh: () => refresh(ref),
+        child: const SingleChildScrollView(
+          child: Column(
+            children: [
+              /// 날짜 그리드
+              DatesGrid(),
+            ],
+          ),
         ),
       ),
     );
