482f25edd72f34282ca0039f5b280e75a0de683f
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-28 12:35:43 +0900

    Update CalendarList 프로바이더 스트림 프로바이더로 변경

--- lib/provider/calendar_list_provider.dart
+++ lib/provider/calendar_list_provider.dart
@@ -3,13 +3,9 @@ import 'package:flutter_todaily/model/calendar_model.dart';
 import 'package:flutter_todaily/provider/repository/calendar_repository_provider.dart';
 import 'package:flutter_todaily/provider/year_provider.dart';
 
-final calendarListProvider = FutureProvider<List<CalendarModel>>(
-  (ref) async {
-    final selectedYear = ref.watch(yearProvider);
-    final repository = ref.watch(calenarRepositoryProvider);
+final calendarListProvider = StreamProvider<List<CalendarModel>>((ref) {
+  final selectedYear = ref.watch(yearProvider);
+  final repository = ref.watch(calenarRepositoryProvider);
 
-    final result = await repository.selectCalendarListInYear(selectedYear);
-
-    return result ?? [];
-  },
-);
+  return repository.watchCaledarListWithYear(selectedYear);
+}, name: 'CalendarListProvider');
