0dc9b9a93fa316e9189b985c89ff2a5ccbf36cda
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-28 13:37:49 +0900

    Update 월 표시바 상단 고정되도록 수정

--- lib/screen/year_calendar/main_calendar_screen.dart
+++ lib/screen/year_calendar/main_calendar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todaily/screen/year_calendar/widget/month_bar.dart';
 import 'package:flutter_todaily/screen/year_calendar/widget/year_calendar.dart';
 import 'package:flutter_todaily/screen/year_calendar/widget/year_select_bar.dart';
 
@@ -18,10 +19,13 @@ class MainCalendarScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Column(
-        children: const [
+      body: const Column(
+        children: [
           /// 연도 선택 바
           YearSelectBar(),
+
+          /// 월 표시 바
+          MonthBar(),
 
           /// 연도 캘린더
           Expanded(child: YearCalendar()),
--- lib/screen/year_calendar/widget/year_calendar.dart
+++ lib/screen/year_calendar/widget/year_calendar.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todaily/screen/year_calendar/widget/dates_grid.dart';
-import 'package:flutter_todaily/screen/year_calendar/widget/month_bar.dart';
 
 class YearCalendar extends StatelessWidget {
   const YearCalendar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
       child: SingleChildScrollView(
         child: Column(
-          children: const [
-            /// 월 표시 바
-            MonthBar(),
-
+          children: [
             /// 날짜 그리드
             DatesGrid(),
           ],
