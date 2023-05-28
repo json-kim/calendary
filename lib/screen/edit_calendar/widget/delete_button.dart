f6da97e7ffb6a94ed90415823600482a062c0526
jsonkim@cashwalk.io <flutter_dart@kakao.com>
2023-05-28 13:59:45 +0900

    Update 캘린더 삭제전, 다이얼로그 확인 로직 추가

--- lib/screen/edit_calendar/widget/delete_button.dart
+++ lib/screen/edit_calendar/widget/delete_button.dart
@@ -2,7 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todaily/provider/edit_calendar_provider.dart';
 import 'package:flutter_todaily/provider/selected_calendar_provider.dart';
+import 'package:flutter_todaily/util/dialog_utils.dart';
 import 'package:flutter_todaily/util/toast_utils.dart';
+import 'package:flutter_todaily/util/widget/check_alert_dialog.dart';
 
 class DeleteButton extends ConsumerWidget {
   const DeleteButton({super.key});
@@ -20,13 +22,29 @@ class DeleteButton extends ConsumerWidget {
     }
   }
 
+  /// 삭제 전 확인 다이얼로그 콜백
+  Future<bool> checkDeleteDialog(BuildContext context) async {
+    return await showDialog<bool?>(
+          context: context,
+          builder: (context) => const CheckAlertDialog(
+            title: DialogUtils.deleteTitle,
+            content: DialogUtils.deleteContent,
+          ),
+        ) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Visibility(
         visible: ref.watch(selectedCalendarProvider
             .select((calendar) => calendar.value != null)),
         child: IconButton(
-            onPressed: () => deleteCalendar(ref),
+            onPressed: () async {
+              final check = await checkDeleteDialog(context);
+              if (!check) return;
+              deleteCalendar(ref);
+            },
             icon: const Icon(Icons.delete_outline)));
   }
 }
